1cfbb977936cff66a17d4a90cac66149120548d4
DiyanLyubchev <Diyan.Lyubchev@gmail.com>
2023-03-11 16:28:35 +0200

    Initial commit

---
+++ ConsoleAppBackgroundTask/ConsoleAppBackgroundTask/BackgroundTask.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace ConsoleAppBackgroundTask;
+
+public sealed class BackgroundTask : IHostedService, IDisposable
+{
+    private Timer? timer;
+
+    public Task StartAsync(CancellationToken stoppingToken)
+    {
+        this.timer = new Timer(RunService, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+
+        return Task.CompletedTask;
+    }
+
+    private void RunService(object state)
+    {
+        Console.WriteLine($"Time: {DateTime.Now:HH:mm:ss}");
+    }
+
+    public Task StopAsync(CancellationToken stoppingToken)
+    {
+        this.timer?.Change(Timeout.Infinite, 0);
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        this.timer?.Dispose();
+    }
+}
+
---
+++ ConsoleAppBackgroundTask/ConsoleAppBackgroundTask/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppBackgroundTask;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+                .ConfigureHostConfiguration(config => { })
+                .ConfigureServices((context, service) =>
+                {
+                    service.AddHostedService<BackgroundTask>();
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+host.Run();
+
